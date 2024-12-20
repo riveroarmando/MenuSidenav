ec03b133df852e209ee7a67aa98ada7a9a95ceda
Armando Rivero <armando.rivero@ricoh-la.com>
2024-12-20 10:38:35 -0300

    Menu Funcionando Angular Material 17

---
+++ src/app/app.component.ts
@@ -0,0 +1,26 @@
+import { Component, computed, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+/* Angular Material */
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+
+/* Componentes custom para el sidenav */
+import { CustomSidenavComponent } from "./components/custom-sidenav/custom-sidenav.component";
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, MatToolbarModule, MatSidenavModule, MatButtonModule, MatIconModule, CustomSidenavComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  
+  /* Codigo para la parte de colapsar el menu */
+  collapsed = signal(false);
+  sidenavWidth = computed(()=> this.collapsed() ? '65px': '250px');
+
+}
